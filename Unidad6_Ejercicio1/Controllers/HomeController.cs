e97e5f0f59b6a693bd9d7dd6e99b56cecccafbb0
aleleotta <114069815+aleleotta@users.noreply.github.com>
2023-10-25 13:10:03 +0200

    .

---
+++ Unidad6_Ejercicio1/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Unidad6_Ejercicio1.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Unidad6_Ejercicio1/Controllers/ProductsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Unidad6_Ejercicio1.Controllers
+{
+    public class ProductsController : Controller
+    {
+
+        public IActionResult ProductsList()
+        {
+            return View();
+        }
+
+    }
+}
